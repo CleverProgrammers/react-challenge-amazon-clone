d89fa3fd0dba8686931af12bde02150e1c84c123
Neel Patel <62853591+neelpatel23@users.noreply.github.com>
2021-03-15 16:06:44 -0500

    Update Order.js

--- src/Order.js
+++ src/Order.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Order.css'
+import React from 'react';
+import './Order.css';
 import moment from "moment";
 import CheckoutProduct from "./CheckoutProduct";
 import CurrencyFormat from "react-currency-format";
